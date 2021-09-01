c6b37a256855c57466867be031137a9d123ce1e0
Ian Cohen <ian_cohen90@live.com>
2021-09-01 15:19:22 -0400

    connected database to server

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Ijc@6390@',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
     })
+})
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 })
 
 app.use((req, res) => {
